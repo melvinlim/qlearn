9920d391d13f8f2b6303962d4d866f19ccba2c80
Melvin Lim <melvinlim@MacBook.local>
2018-05-22 19:47:55 -0700

    finish converting to submodule

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup, Extension
 
 module1 = Extension('qlearn',
-                    sources = ['main.cpp','player.cpp','qfunction.cpp','mynn/layer.cpp','mynn/net.cpp','mynn/idx.cpp'])
+                    sources = ['main.cpp','player.cpp','qfunction.cpp','nnet/layer.cpp','nnet/net.cpp','nnet/idx.cpp'])
 
 setup (name = 'QLearn',
        version = '1.0',
